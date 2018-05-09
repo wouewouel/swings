b2f219eb10af2ea61b590091961fff45aee2be1e
wouewouel <ouriel.sebbagh@epfl.ch>
2018-05-09 18:38:10 +0200

    pour l'instant, en attend de voir un assist

---
+++ P9/TestDoubleRessort.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "DoubleRessort.h"
+using namespace std;
+
+int main()
+{
+	Vecteur a(0.8,0,0.6);
+	Vecteur P(0.12,0.18);
+	Vecteur Q(0.13,0.15);
+	DoubleRessort doubleressort(1,1,1,1,1,1,a,P,Q);
+	cout<<doubleressort;
+	return 0;
+}
